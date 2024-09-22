e68518b301c193b4e2e56f97952c45af629d2733
Rolando Quiros <107447206+MCQuire08@users.noreply.github.com>
2024-09-21 20:41:57 -0600

    Initial commit

---
+++ src/main/java/com/project/tarea1/logic/entity/game/GameRepository.java
@@ -0,0 +1,6 @@
+package com.project.tarea1.logic.entity.game;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
---
+++ src/main/java/com/project/tarea1/logic/entity/rol/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.project.tarea1.logic.entity.rol;
+
+public enum RoleEnum {
+    USER,
+    ADMIN,
+    SUPER_ADMIN
+}
